ddff392248b8e24cdb72531a638a9267bdc01f9d
harold <harold@thinkpad.localdomain>
2022-03-20 17:17:29 +0100

    Applied iso-14755 patch

--- config.h
+++ config.h
@@ -220,6 +220,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_K,           kscrollup,      {.i =  1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ TERMMOD,              XK_J,           kscrolldown,    {.i =  1} },
+	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
 };
 
 /*
