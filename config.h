7b9310cee8e36ebd9e37102c97e9fc55511ae2e5
harold <harold@thinkpad.localdomain>
2022-04-09 03:31:26 +0200

    Changed to Hack font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono Nerd Font Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Hack:pixelsize=14:antialias=true:autohint=true";
 
 /* disable bold, italic and roman fonts globally */
 int disablebold = 0;
@@ -15,7 +15,6 @@ int disableroman = 0;
 /* Spare fonts */
 static char *font2[] = {
     "JoyPixels:pixelsize=12:antialias=true:autohint=true",
-    "Symbols Nerd Font:pixelsize=12:antialias=true:autohint=true"
 };
 
 static int borderpx = 2;
